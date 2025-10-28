30920235449da58648e321db18f4a5a2dc713b7b
vaes01 <218143640+vaes01@users.noreply.github.com>
2025-10-28 13:37:46 -0300

    Finished version of the Etch-a-Sketch project

---
+++ script.js
@@ -0,0 +1,61 @@
+
+let mainContainer = document.querySelector("#mainContainer"); 
+
+let newSketchBtn = document.querySelector("#newSketchBtn");
+
+buildGrid(30);
+paintGrid();
+
+newSketchBtn.addEventListener("click", () => {
+
+    let pixels = 10;
+
+    for (let i = 1; i > 0; i++) {
+        pixels = prompt("How many squares per side? 10 minimum and 100 maximum.");
+
+        if (pixels >= 10 && pixels <= 100) {
+            i = -1;
+        } else {
+            alert("Please select a number of squares between 10 and 100.");
+        }
+    }
+
+    removeContainerRows = document.querySelectorAll(".containerRows");
+    removeContainerRows.forEach(element => {
+        element.remove();
+    });
+
+    buildGrid(pixels);
+    paintGrid();
+
+});
+
+
+function buildGrid(pixels) {
+
+    for (let i = 1; i <= pixels; i++) {
+        let containerRow = document.createElement("div")
+        containerRow.classList.add("containerRows");
+        mainContainer.appendChild(containerRow);
+
+        for (let j = 1; j <= pixels; j++) {
+
+            let gridElement = document.createElement("div");
+            gridElement.classList.add("gridElements");
+            containerRow.appendChild(gridElement);
+
+        }
+    };
+};
+
+function paintGrid() {
+
+    let allGridElements = document.querySelectorAll(".gridElements");
+
+    allGridElements.forEach(element => {
+        element.addEventListener("mouseenter", () => {
+            element.style.backgroundColor = "black";
+        });
+    });
+};
+     
